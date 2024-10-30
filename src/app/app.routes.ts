a5bd300a40d175d0abc641603e5c27803ab81ab0
juan josé pérez plaza <juanjo.perez.plaza@gmail.com>
2024-10-30 12:04:07 +0100

    configuracion inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.routes').then(m => m.HOME_ROUTES)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+];
